153b0b5cd58471c8409dbfe89c224ce67822e39d
HeeFlower5001 <gmltmd3tp@naver.com>
2025-10-09 16:22:27 +0900

    fix: hilt build 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
     id("kotlin-kapt")
 }
