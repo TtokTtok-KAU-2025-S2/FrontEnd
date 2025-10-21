6da88d9e5678abf335c04dd650f5ffb5efc668ac
HeeFlower5001 <gmltmd3tp@naver.com>
2025-10-22 01:15:34 +0900

    feat: Navigator 작성
    - XML + Jetpack 각 페이지를 혼용하기 위한 nav_graph.xml 작성
    - XML -> ComposeView를 보이기 위한 Activity_main.xml, MainActivity.kt 수정

---
+++ app/src/main/java/com/kau/ttokttok/ui/compose/login/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.kau.ttokttok.ui.compose.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+        )
+
+        setContent {
+            LoginRoute()
+        }
+    }
+}
