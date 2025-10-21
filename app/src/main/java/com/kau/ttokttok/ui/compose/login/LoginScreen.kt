9a45aafcbc15fc2909d7cacbcdda039fbec9d5ff
HeeFlower5001 <gmltmd3tp@naver.com>
2025-10-22 03:22:20 +0900

    design: MainScreen 초안 작성
    - GlassCard 카드 컴포넌트화
    - Starfield 배경 컴포넌트화
    - MainScreen 초안 작성

--- app/src/main/java/com/kau/ttokttok/ui/compose/login/LoginScreen.kt
+++ app/src/main/java/com/kau/ttokttok/ui/compose/login/LoginScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kau.ttokttok.ui.component.common.background.StarField
 import kotlin.random.Random
 
 @Composable
@@ -79,7 +80,6 @@ fun LoginScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .alpha(0.35f),
-            starCount = 120 // 별 개수 조절
         )
 
         // 페이드 보카시
@@ -218,37 +218,6 @@ fun LoginScreen(
         }
     }
 }
-
-@Composable
-private fun StarField(
-    modifier: Modifier = Modifier,
-    starCount: Int
-) {
-    val stars = remember {
-        List(starCount) {
-            Star(
-                x = Random.nextFloat(),
-                y = Random.nextFloat(),
-                r = Random.nextFloat() * 2f + 0.5f,
-                a = Random.nextFloat() * 0.5f + 0.5f
-            )
-        }
-    }
-
-    Canvas(modifier = modifier) {
-        val w = size.width
-        val h = size.height
-        stars.forEach { s ->
-            drawCircle(
-                color = Color.White.copy(alpha = s.a),
-                radius = s.r,
-                center = Offset(s.x * w, s.y * h)
-            )
-        }
-    }
-}
-
-private data class Star(val x: Float, val y: Float, val r: Float, val a: Float)
 
 @Composable
 private fun FrostedPanel(
